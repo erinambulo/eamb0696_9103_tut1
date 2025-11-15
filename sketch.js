936db35da2c5e04fd5580317a2e634b2e7138879
erinambulo <eamb0696@uni.sydney.edu.au>
2025-11-15 17:08:21 +1100

    FINAL-sketch.js

--- sketch.js
+++ sketch.js
@@ -9,8 +9,8 @@ let bgImg, bullImg;
 let bgSegments = [];
 let bullSegments = [];
 
-let gridSize = 30; // how many grid cells across and down
-let shapeSize = 3.0; // size of each shape (multiplier)
+let gridSize = 75; // how many grid cells across and down
+let shapeSize = 1.75; // size of each shape (multiplier)
 let backColor = 250; // backrgound colour (light grey)
 
 // =====================================
